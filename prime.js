d0c10c8357d13753b6e885badb9a176d286d93e1
SURAJ SHIMPI JI <surajshimpi29@gmail.com>
2022-12-10 01:28:58 +0530

    created prime file

---
+++ prime.js
@@ -0,0 +1,14 @@
+function prime(num){
+	let count=0;
+	for(let i=1; i<=num; i++){
+		if(num%i==0){
+			                            //    count++
+		}
+	}
+
+	if(count==2){				
+		console.log("Prime")
+	}else{
+		console.log("Not prime")
+	}
+}
